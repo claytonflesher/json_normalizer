38330ef7ccdd2fcaa0af5a3c0a75325d78285635
Calvyn82 <claytonflesher@gmail.com>
2015-09-28 20:21:54 -0500

    working proof of concept

--- lib/normalizer.rb
+++ lib/normalizer.rb
@@ -1,3 +1,28 @@
-class Normalizer
+require "json"
 
+class Normalizer
+  def initialize(data: )
+    @parsed     = JSON.parse(data)
+    @exclusions = @parsed.keys
+    @json       = normalize(data: @parsed)
+  end
+
+  attr_reader :json, :exclusions
+  private :exclusions
+
+  private
+
+  def normalize(data:)
+    data.delete_if do |key, value|
+      exclusions.select { |element| element == key }.count > 1
+    end
+    data.each_key { |key| exclusions << key }
+    data.each do |key, value|
+      if value.class == Array
+        value.map { |element| normalize(data: element) }
+        value.delete({})
+      end
+    end
+    data.to_json
+  end
 end
--- spec/normalizer_spec.rb
+++ spec/normalizer_spec.rb
@@ -1,5 +1,39 @@
 require_relative "../lib/normalizer.rb"
+require "json"
 
 describe "Normalizer" do
+  before(:each) do
+    @json = {
+      "wholesaler" => "US Foods",
+      "delivered" =>"2015-06-19T05:15:00-0500",
+      "contacts" => [
+        {
+          "wholesaler" => "US Foods",
+          "name" => "John Lederer"
+        },
+        {
+          "wholesaler" => "Sysco",
+          "name" => "Bill Delaney"
+        }
+      ]
+    }.to_json
+    @normalizer = Normalizer.new(data: @json)
+  end
 
+  it "returns normalized json" do
+    expect(@normalizer.json).to eq(
+      {
+        "wholesaler" => "US Foods", 
+        "delivered"  => "2015-06-19T05:15:00-0500",
+        "contacts"   => [
+          {
+            "name" => "John Lederer"
+          },
+          {
+            "name" => "Bill Delaney"
+          }
+        ]
+      }.to_json
+    )
+  end
 end
